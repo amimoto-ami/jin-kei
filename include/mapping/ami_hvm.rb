6a35788a693e9113a472e8e400daa39aa2bd3d1a
hideokamoto <hide.okamoto@digitalcube.jp>
2016-02-26 13:39:00 +0900

    update ami AMIMOTO HVM

--- include/mapping/ami_hvm.rb
+++ include/mapping/ami_hvm.rb
@@ -1,15 +1,15 @@
-{"eu-west-1"=>"ami-2b75a958", "ap-southeast-1"=>"ami-ff86419c", "ap-southeast-2"=>"ami-7085da13", "eu-central-1"=>"ami-a6bcacca", "ap-northeast-2"=>"ami-259d534b", "ap-northeast-1"=>"ami-e6da8088", "us-east-1"=>"ami-7c166716", "sa-east-1"=>"ami-12338b7e", "us-west-1"=>"ami-b2b4d8d2", "us-west-2"=>"ami-2874991b"}
+{"eu-west-1"=>"ami-3b289a48", "ap-southeast-1"=>"ami-39945d5a", "ap-southeast-2"=>"ami-9f93b4fc", "eu-central-1"=>"ami-181a0074", "ap-northeast-2"=>"ami-4c2be522", "ap-northeast-1"=>"ami-d42c2dba", "us-east-1"=>"ami-0c417066", "sa-east-1"=>"ami-da0083b6", "us-west-1"=>"ami-efafde8f", "us-west-2"=>"ami-2835d648"}
 MPAmimotov4(
   {
-  "eu-west-1"=>{"AMI"=>"ami-2b75a958"},
-  "ap-southeast-1"=>{"AMI"=>"ami-ff86419c"},
-  "ap-southeast-2"=>{"AMI"=>"ami-7085da13"},
-  "eu-central-1"=>{"AMI"=>"ami-a6bcacca"},
-  "ap-northeast-2"=>{"AMI"=>"ami-259d534b"},
-  "ap-northeast-1"=>{"AMI"=>"ami-e6da8088"},
-  "us-east-1"=>{"AMI"=>"ami-7c166716"},
-  "sa-east-1"=>{"AMI"=>"ami-12338b7e"},
-  "us-west-1"=>{"AMI"=>"ami-b2b4d8d2"},
-  "us-west-2"=>{"AMI"=>"ami-2874991b"},
+  "eu-west-1"=>{"AMI"=>"ami-3b289a48"},
+  "ap-southeast-1"=>{"AMI"=>"ami-39945d5a"},
+  "ap-southeast-2"=>{"AMI"=>"ami-9f93b4fc"},
+  "eu-central-1"=>{"AMI"=>"ami-181a0074"},
+  "ap-northeast-2"=>{"AMI"=>"ami-4c2be522"},
+  "ap-northeast-1"=>{"AMI"=>"ami-d42c2dba"},
+  "us-east-1"=>{"AMI"=>"ami-0c417066"},
+  "sa-east-1"=>{"AMI"=>"ami-da0083b6"},
+  "us-west-1"=>{"AMI"=>"ami-efafde8f"},
+  "us-west-2"=>{"AMI"=>"ami-2835d648"},
  }
 )
