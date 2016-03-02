bc94efb57ca62d809c396176059c2625346c1a90
hideokamoto <hide.okamoto@digitalcube.jp>
2016-03-02 20:02:06 +0900

    template update

--- 001-woo.rb
+++ 001-woo.rb
@@ -7,29 +7,6 @@ EOS
 
 Mappings do
   _include "include/mapping/ami_woo.rb"
-  _include "include/mapping/az.rb"
 end
 
-Parameters do
-  _include "params/keyname.rb"
-  _include "params/ssh_location.rb"
-  _include "params/ec2_instance.rb"
-  _include "params/rds_settings.rb"
-end
-
-Resources do
-  _include 'include/vpc/vpc.rb'
-  _include "include/ec2/ec2-for-woo.rb"
-  _include "include/rds/rds.rb"
-  _include "include/security_group.rb"
-  _include "include/elb.rb"
-  _include 'include/iam/for_stack_001.rb'
-  _include "include/cloudfront/cf.rb"
-  _include "include/s3/s3-media.rb"
-end
-
-Outputs do
-  _include "outputs/ec2.rb"
-  _include "outputs/cloudfront.rb"
-  _include "outputs/s3.rb"
-end
+_include "001-base.rb"
