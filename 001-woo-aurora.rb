106bc77a08f8db8b6c96b77ef0be9ac333e45200
hideokamoto <hide.okamoto@digitalcube.jp>
2016-03-10 20:24:51 +0900

    add template that support aurora

---
+++ 001-woo-aurora.rb
@@ -0,0 +1,21 @@
+AWSTemplateFormatVersion "2010-09-09"
+Description (<<-EOS).undent
+  This is Powerful & Scalable AMIMOTO Stack made by CloudFormation.
+  Added RDS, Database change more scalable.
+  Use service is VPC,EC2,EIP,CloudFront,S3,RDS(Amazon Aurora).
+EOS
+
+Mappings do
+  _include "include/mapping/ami_woo.rb"
+end
+
+Conditions do
+  CreateSecDB do
+    Fn__Equals [
+      _{ Ref "MulitiAZDatabase" },
+      true
+    ]
+  end
+end
+
+_include "001-base-aurora.rb"
