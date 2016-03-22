64986103138e963e21b0f0187f9b0bbfecf8bcf9
sawanoboly <sawanoboriyu@higanworks.com>
2016-03-22 15:33:38 +0900

    CodeCommit Parameters

---
+++ params/codecommit_arn.rb
@@ -0,0 +1,6 @@
+CodeCommitARN do
+  Description "Your Codecommit ARN (Caution: It will be override by wordpress.)"
+  Type        "String"
+  AllowedPattern "arn:aws:codecommit:.*"
+  ConstraintDescription : "must be an CodeCommit Repository ARN"
+end
