5001cacf6959d438ed6dda6f20da299ac7ec1aa0
hideokamoto <hide.okamoto@digitalcube.jp>
2016-04-22 21:56:03 +0900

    auto set up s3 plugin

--- include/ec2/metadata-for-rds.rb
+++ include/ec2/metadata-for-rds.rb
@@ -6,6 +6,12 @@ _path("/opt/aws/cloud_formation.json") do
 		"password" : "{{password}}",
 		"endpoint" : "{{endpoint}}",
 		"port"     : 3306
+	  },
+	  "s3_conf": {
+		  "region": "{{s3region}}",
+		  "bucket": "{{s3bucket}}",
+		  "url": "{{s3url}}",
+		  "storage": "STANDARD"
 	  }
 	}'
   context do
@@ -16,6 +22,15 @@ _path("/opt/aws/cloud_formation.json") do
 	  Ref "MySQLPassword"
 	end
 	serverid "dummy(value_will_update_by_AmimotoFrontLC)"
+	s3bucket do
+	  Ref "S3Media"
+	end
+	s3url do
+	  Fn__GetAtt "S3Media", "WebsiteURL"
+	end
+	s3region do
+	  Ref "AWS::Region"
+	end
   end
   mode "00644"
   owner "root"
