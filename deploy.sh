5a6b9ef52387b7a8fac6b5ca7d98307fa91d6422
hideokamoto <hide.okamoto@digitalcube.jp>
2016-04-25 17:50:17 +0900

    convert script

---
+++ deploy.sh
@@ -0,0 +1,9 @@
+#/bin/bash
+kumogata convert 001.rb > json/001.json
+kumogata convert 001-woo.rb > json/001-woo.json
+kumogata convert 001-mod.rb > json/001-mod.json
+kumogata convert 001-hhvm.rb > json/001-hhvm.json
+kumogata convert 001-hvm-aurora.rb > json/001-hvm-aurora.json
+kumogata convert 001-woo-aurora.rb > json/001-woo-aurora.json
+kumogata convert 001-mod-aurora.rb > json/001-mod-aurora.json
+kumogata convert 001-hhvm-aurora.rb > json/001-hhvm-aurora.json
