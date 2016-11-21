df79e15e4b2646ee97c6c5efa4431698791449bd
hideokamoto <hide.okamoto@digitalcube.jp>
2016-11-21 11:15:45 +0900

    add new image (HVM PHP7 ready)

--- bin/validate.sh
+++ bin/validate.sh
@@ -9,3 +9,5 @@ kumogata validate 001-hhvm-aurora.rb
 kumogata validate 001-woo-hvm.rb
 kumogata validate 001-woo-hhvm.rb
 kumogata validate 001-woo-mod.rb
+kumogata validate 001-hvm-php7.rb
+kumogata validate 001-hvm-php7-aurora.rb
