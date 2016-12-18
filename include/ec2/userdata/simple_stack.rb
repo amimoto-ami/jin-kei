e32007ef42ed0cd1daede196c78c1b097a652fab
hideokamoto <hide.okamoto@digitalcube.jp>
2016-12-18 17:50:24 +0900

    change indent

--- include/ec2/userdata/simple_stack.rb
+++ include/ec2/userdata/simple_stack.rb
@@ -2,20 +2,21 @@
 	Fn__Join [
 	  "",
 	  [
-		"#!/bin/bash\n",
-		"/opt/aws/bin/cfn-init -s ",
-		_{
-		  Ref "AWS::StackName"
-		},
-		" -r EC2 ",
-		" --region ",
-		_{
-		  Ref "AWS::Region"
-		},
-		"\n",
-		"until find /var/www/vhosts -name wp-config.php  ; do sleep 5 ; done", "\n",
-		"/opt/aws/bin/cfn-signal -e $? -r \"WordPress setup complete\" '",
-		_{ Ref "EC2WaitHandle" }, "'\n"
+			"#!/bin/bash\n",
+
+			"/opt/aws/bin/cfn-init -s ",
+			_{
+				Ref "AWS::StackName"
+			},
+			" -r EC2 ",
+			" --region ",
+			_{
+				Ref "AWS::Region"
+			},
+			"\n",
+			"until find /var/www/vhosts -name wp-config.php  ; do sleep 5 ; done", "\n",
+			"/opt/aws/bin/cfn-signal -e $? -r \"WordPress setup complete\" '",
+			_{ Ref "EC2WaitHandle" }, "'\n"
 	  ]
 	]
   end
