4f73ba7dab02dc990ee0be8802a33b4b83219249
hideokamoto <hide.okamoto@digitalcube.jp>
2016-12-26 19:49:24 +0900

    show-elb-domain for ssh

--- outputs/001-base-output.rb
+++ outputs/001-base-output.rb
@@ -1,6 +1,13 @@
 Outputs do
   if $Stack_Type != 'autoscale' then
     _include "outputs/ec2.rb"
+  else
+    ELBDomain do
+        Description "Instance ID required for first login. Please make a note of this value."
+        Value do
+            Fn__GetAtt "ELB", "DNSName"
+        end
+    end
   end
   _include "outputs/cloudfront.rb"
   _include "outputs/s3.rb"
