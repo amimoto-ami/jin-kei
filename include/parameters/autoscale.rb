bf31b715022ffd046d4308c2b81f1b6a96ec5ad0
hideokamoto <hide.okamoto@digitalcube.jp>
2017-01-10 20:45:16 +0900

    change default num

--- include/parameters/autoscale.rb
+++ include/parameters/autoscale.rb
@@ -7,11 +7,11 @@ end
 AutoScalingMinSize do
   Description "Min instance number of AutoScaling group"
   Type        "Number"
-  Default     2
+  Default     1
 end
 
 AutoScalingDesiredCapacity do
   Description "Desired capacity of AutoScaling group"
   Type        "Number"
-  Default     2
+  Default     1
 end
