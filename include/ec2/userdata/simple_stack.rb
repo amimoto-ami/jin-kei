ef2e5e18f12d8d4715fdc2c5e72bbf2e426a0990
hideokamoto <hide.okamoto@digitalcube.jp>
2017-01-24 19:02:23 +0900

    woo hvm update

--- include/ec2/userdata/simple_stack.rb
+++ include/ec2/userdata/simple_stack.rb
@@ -8,7 +8,7 @@
 			"yum install -y aws-cfn-bootstrap\n",
 
 			"# Start cfn-init\n",
-      "/opt/aws/bin/cfn-init -s ",
+			"/opt/aws/bin/cfn-init -s ",
 			_{
 				Ref "AWS::StackName"
 			},
@@ -21,6 +21,24 @@
 
 			"# Start up the cfn-hup daemon to listen for changes to the EC2 instance metadata\n",
 			"/opt/aws/bin/cfn-hup || error_exit 'Failed to start cfn-hup'\n",
+
+			## Set CFN Flag
+			"tmp_json=`mktemp`\n",
+			"amimoto_json='/opt/local/amimoto.json'\n",
+			"json='{",
+			"\"wordpress\":{",
+				"\"jinkei_cf\": \"true\",",
+			"}",
+			"}'\n",
+
+			"[ ! -e /opt/local ] && /bin/mkdir -p /opt/local\n",
+			"if [ -f $amimoto_json ]; then\n",
+			"  hash jq || sudo /usr/bin/yum install -y jq\n",
+			"  /usr/bin/jq -s '.[0] * .[1]' $amimoto_json <(echo $json) > $tmp_json\n",
+			"else\n",
+			"  echo $json > $tmp_json\n",
+			"fi\n",
+
 
 			"\n",
 			"until find /var/www/vhosts -name wp-config.php  ; do sleep 5 ; done", "\n",
