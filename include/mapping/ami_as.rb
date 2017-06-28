565600ddf92e3a98ab41def0e5700d1f61ef00df
hideokamoto <hide.okamoto@digitalcube.jp>
2017-06-28 09:45:20 +0900

    remove unuse region

--- include/mapping/ami_as.rb
+++ include/mapping/ami_as.rb
@@ -1,6 +1,5 @@
 MPAmimotov4(
   {
-  "eu-central-1"=>{"AMI"=>"ami-841cc0eb"},
   "us-east-1"=>{"AMI"=>"ami-27244131"},
   "us-west-2"=>{"AMI"=>"ami-928e10f2"},
   "eu-west-1"=>{"AMI"=>"ami-cc1c1caa"},
