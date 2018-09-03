5446ee25b95a889740254d3eb9601b1467d4a129
hideokamoto <hide.okamoto@digitalcube.jp>
2018-09-03 10:07:08 +0900

    support t3 instances

--- bin/self_convert.sh
+++ bin/self_convert.sh
@@ -1,12 +1,9 @@
 #/bin/bash
 kumogata convert 001.rb > json/001.json
 kumogata convert 001-mod.rb > json/001-mod.json
-kumogata convert 001-hhvm.rb > json/001-hhvm.json
 kumogata convert 001-hvm-aurora.rb > json/001-hvm-aurora.json
 kumogata convert 001-woo-aurora.rb > json/001-woo-aurora.json
 kumogata convert 001-mod-aurora.rb > json/001-mod-aurora.json
-kumogata convert 001-hhvm-aurora.rb > json/001-hhvm-aurora.json
-kumogata convert 001-woo-hhvm.rb > json/001-woo.json
 kumogata convert 001-woo-hvm.rb > json/001-woo-hvm.json
 kumogata convert 001-woo-mod.rb > json/001-woo-mod.json
 kumogata convert as-hvm.rb > json/as-hvm.json
--- bin/validate.sh
+++ bin/validate.sh
@@ -1,12 +1,9 @@
 #/bin/bash
 kumogata validate 001.rb
 kumogata validate 001-mod.rb
-kumogata validate 001-hhvm.rb
 kumogata validate 001-hvm-aurora.rb
 kumogata validate 001-woo-aurora.rb
 kumogata validate 001-mod-aurora.rb
-kumogata validate 001-hhvm-aurora.rb
 kumogata validate 001-woo-hvm.rb
-kumogata validate 001-woo-hhvm.rb
 kumogata validate 001-woo-mod.rb
 kumogata validate as-hvm.rb
